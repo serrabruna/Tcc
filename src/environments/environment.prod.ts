5dab4ab86d161a65f1cbb7463c96e35c63c9860c
serrabruna <125033654+serrabruna@users.noreply.github.com>
2023-06-02 13:36:15 +0000

    Tela de Login e Registro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+   firebaseConfig:{
+    apiKey: "AIzaSyAzRgyh2oy6F6hgYqYJmw26RBnfXRnXHSw",
+    authDomain: "aula-eec40.firebaseapp.com",
+    projectId: "aula-eec40",
+    storageBucket: "aula-eec40.appspot.com",
+    messagingSenderId: "82969912700",
+    appId: "1:82969912700:web:9c33f386da808d04fdf0f9",
+    measurementId: "G-L323RVBD95"
   },
   production: true
 };
