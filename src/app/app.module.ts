bd360f4cbe1af889ee707b1b51ccd298ed81a8e3
souzastef <145347476+souzastef@users.noreply.github.com>
2023-09-22 10:25:44 +0000

    Att teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
+      
 
 @NgModule({
   declarations: [AppComponent],
